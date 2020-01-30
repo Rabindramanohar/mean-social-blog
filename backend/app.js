3b6d03c045023ccb65c28045f48cac61eeedcf18
Rabindra Mahato <robinmanohar@gmail.com>
2020-01-30 23:55:40 +0530

    till api creation 39

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,24 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('middle ware!!');
-    
-    next();
-}) 
 
-app.use((req, res, next) => {
-    res.send('hello from express!!');
-})
+app.use("/api/posts",(req, res, next) => {
+    const post = [
+    {
+        id: "fhdskjfdsklfj",
+        title: "My first post",
+        content: "First post is here"
+    }, 
+    {
+        id: "fdsfsfsfdsfdsf",
+        title: "My second post",
+        content: "Second post is here!!"
+    }
+];
+    res.status(200).json({
+        message: 'Post fetch successfully!!',
+        posts: post
+    });
+});
 
 module.exports = app;
